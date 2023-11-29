5e420c0caa30b2e82613bb0d4a0a75c8da5070eb
hzx <hzx@gmail.com>
2023-11-29 18:53:20 +0800

    1

--- application/admin/controller/sys/Report.php
+++ application/admin/controller/sys/Report.php
@@ -72,4 +72,40 @@ class Report extends Backend
         $result = ['total' => $list->total(), 'rows' => $list->items()];
         return json($result);
     }
+
+    public function index3()
+    {
+        //设置过滤方法
+        $this->request->filter(['strip_tags', 'trim']);
+        if (false === $this->request->isAjax()) {
+            return $this->view->fetch();
+        }
+        //如果发送的来源是 Selectpage，则转发到 Selectpage
+        if ($this->request->request('keyField')) {
+            return $this->selectpage();
+        }
+        [$where, $sort, $order, $offset, $limit] = $this->buildparams();
+        $list = $this->model
+            ->where($where)
+            ->order($sort, $order)
+            ->paginate($limit);
+        foreach ($list as &$value){
+            $start = strtotime("{$value['date']} 00:00:00");
+            $end = strtotime("{$value['date']} 23:59:59");
+            $value['newuser'] = (new User())->where(['sid'=>['<>',0]])->where(['createtime'=>['between',[$start,$end]]])->count();
+            $order = (new FinanceOrder())->where(['is_robot'=>0])->where(['createtime'=>['between',[$start,$end]]])->group('user_id')->column('user_id');
+            $value['neworder'] = (new User())->where(['sid'=>['<>',0],'id'=>['in',$order]])->count();
+            $newrechargenum = (new UserRecharge())->where(['createtime'=>['between',[$start,$end]],'status'=>1])->group('user_id')->column('user_id');
+            $newrechargeuser = (new User())->where(['sid'=>['<>',0],'id'=>['in',$newrechargenum]])->column('id');
+            $value['newrechargenum'] = count($newrechargeuser);
+            $newcashnum = (new UserCash())->where(['createtime'=>['between',[$start,$end]],'status'=>3])->group('user_id')->column('user_id');
+            $newcashuser = (new User())->where(['sid'=>['<>',0],'id'=>['in',$newcashnum]])->column('id');
+            $value['newcashnum'] = count($newcashuser);
+            $value['newrecharge'] = (new UserRecharge())->where(['user_id'=>['in',$newrechargeuser],'createtime'=>['between',[$start,$end]]])->sum('price');
+            $value['newcash'] = (new UserCash())->where(['user_id'=>['in',$newcashuser],'createtime'=>['between',[$start,$end]]])->sum('price');
+        }
+//        var_dump($list);exit;
+        $result = ['total' => $list->total(), 'rows' => $list->items()];
+        return json($result);
+    }
 }
--- public/assets/js/backend/sys/report.js
+++ public/assets/js/backend/sys/report.js
@@ -94,6 +94,51 @@ define(['jquery', 'bootstrap', 'backend', 'table', 'form'], function ($, undefin
             Table.api.bindevent(table);
         },
 
+        index3: function () {
+            // 初始化表格参数配置
+            Table.api.init({
+                extend: {
+                    index_url: 'sys/report/index3' + location.search,
+                    add_url: 'sys/report/add',
+                    edit_url: 'sys/report/edit',
+                    del_url: 'sys/report/del',
+                    multi_url: 'sys/report/multi',
+                    import_url: 'sys/report/import',
+                    table: 'report',
+                }
+            });
+
+            var table = $("#table");
+
+            // 初始化表格
+            table.bootstrapTable({
+                url: $.fn.bootstrapTable.defaults.extend.index_url,
+                pk: 'id',
+                sortName: 'id',
+                fixedColumns: true,
+                fixedRightNumber: 1,
+                columns: [
+                    [
+                        // {checkbox: true},
+                        // {field: 'id', title: __('Id')},
+                        {field: 'date', title: __('Date'), operate: 'LIKE'},
+                        {field: 'newuser', title: __('注册人数')},
+                        {field: 'neworder', title: __('下单人数')},
+                        {field: 'newrechargenum', title: __('充值人数')},
+                        {field: 'newcashnum', title: __('提现人数')},
+                        {field: 'newrecharge', title: __('充值金额')},
+                        {field: 'newcash', title: __('提现金额')},
+                        {field: 'createtime', title: __('Createtime'), operate:'RANGE', addclass:'datetimerange', autocomplete:false, formatter: Table.api.formatter.datetime},
+                        // {field: 'updatetime', title: __('Updatetime'), operate:'RANGE', addclass:'datetimerange', autocomplete:false, formatter: Table.api.formatter.datetime},
+                        // {field: 'operate', title: __('Operate'), table: table, events: Table.api.events.operate, formatter: Table.api.formatter.operate}
+                    ]
+                ]
+            });
+
+            // 为表格绑定事件
+            Table.api.bindevent(table);
+        },
+
         add: function () {
             Controller.api.bindevent();
         },
