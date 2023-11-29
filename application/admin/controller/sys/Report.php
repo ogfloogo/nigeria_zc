5cbcf04236722d4436b5823c100389738c015b44
hzx <hzx@gmail.com>
2023-11-29 17:09:31 +0800

    1

--- application/admin/controller/sys/Report.php
+++ application/admin/controller/sys/Report.php
@@ -2,6 +2,10 @@
 
 namespace app\admin\controller\sys;
 
+use app\admin\model\finance\UserCash;
+use app\admin\model\finance\UserRecharge;
+use app\admin\model\financebuy\FinanceOrder;
+use app\admin\model\User;
 use app\common\controller\Backend;
 
 /**
@@ -33,5 +37,33 @@ class Report extends Backend
      * 需要将application/admin/library/traits/Backend.php中对应的方法复制到当前控制器,然后进行修改
      */
 
-
+    public function index2()
+    {
+        //设置过滤方法
+        $this->request->filter(['strip_tags', 'trim']);
+        if (false === $this->request->isAjax()) {
+            return $this->view->fetch();
+        }
+        //如果发送的来源是 Selectpage，则转发到 Selectpage
+        if ($this->request->request('keyField')) {
+            return $this->selectpage();
+        }
+        [$where, $sort, $order, $offset, $limit] = $this->buildparams();
+        $list = $this->model
+            ->where($where)
+            ->order($sort, $order)
+            ->paginate($limit);
+        foreach ($list as &$value){
+            $value['user'] = (new User())->where(['sid'=>0])->whereTime('createtime',$value['date'])->count();
+            $order = (new FinanceOrder())->where(['is_robot'=>0])->whereTime('createtime',$value['date'])->group('user_id')->column('user_id');
+            $value['order'] = (new User())->where(['sid'=>0,'user_id'=>['in',$order]])->count();
+            $recharge = (new UserRecharge())->whereTime('createtime',$value['date'])->group('user_id')->column('user_id');
+            $value['recharge'] = (new User())->where(['sid'=>0,'user_id'=>['in',$recharge]])->count();
+            $cash = (new UserCash())->whereTime('createtime',$value['date'])->group('user_id')->column('user_id');
+            $value['cash'] = (new User())->where(['sid'=>0,'user_id'=>['in',$cash]])->count();
+        }
+        var_dump($list);exit;
+        $result = ['total' => $list->total(), 'rows' => $list->items()];
+        return json($result);
+    }
 }
